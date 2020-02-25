c0367f986b89ed18d20aa67c11699554cd1e202a
Yoan Vandevelde <yoan.vandevelde.isn@gmail.com>
2020-02-25 11:31:45 +0100

    entity [WIP]

---
+++ src/Entity/Category.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAme(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setAme(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
