399b362c4e586ec3c6218190fd68e3f5acbd04aa
JosselinLuneau <josselinluneau@gmail.com>
2020-02-25 15:41:23 +0100

    init webpack encore

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var Encore = require('@symfony/webpack-encore');
+
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('public/build/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/build')
+    // only needed for CDN's or sub-directory deploy
+    //.setManifestKeyPrefix('build/')
+
+    .addEntry('js/app', './assets/js/app.js')
+    .addEntry('css/app', './assets/scss/app.scss')
+    .cleanupOutputBeforeBuild()
+
+    .enableSourceMaps(!Encore.isProduction())
+    // enables hashed filenames (e.g. app.abc123.css)
+
+    // enables Sass/SCSS support
+    .enableSassLoader()
+;
+
+module.exports = Encore.getWebpackConfig();
