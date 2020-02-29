59d72fad08d7e20df0c871d2a54abc2d15b37144
Josselin Luneau <josselinluneau@gmail.com>
2020-02-29 17:20:49 +0100

    fix: user default role

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -68,7 +68,7 @@ class User implements UserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = RoleManager::ROLE_USER;
+        $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
     }
--- src/Services/RoleManager.php
+++ src/Services/RoleManager.php
@@ -11,7 +11,13 @@ class RoleManager
     const ROLE_COLLECTOR = "COLLECTOR";
     const ROLE_PROFESSIONAL = "PROFESSIONAL";
 
+    const ROLE_DEFAULT = self::ROLE_USER;
     const PREFIX = "ROLE_";
+
+    public function getDefaultRole(): string
+    {
+        return self::PREFIX.self::ROLE_DEFAULT;
+    }
 
     public function getRole(string $role): string
     {
