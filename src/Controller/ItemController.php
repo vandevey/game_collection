ac78ea8e2e7fcb16b80858e70eeea3737e1a3c30
Josselin Luneau <josselinluneau@gmail.com>
2020-04-29 18:48:28 +0200

    Feat : new item

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -27,17 +27,14 @@ class ItemController extends AbstractController
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            // $form->getData() holds the submitted values
-            // but, the original `$task` variable has also been updated
+            /** @var Item $item */
             $item = $form->getData();
+            $item->setCreatedAt();
+            $item->setUpdatedAt();
 
-            // ... perform some action, such as saving the task to the database
-            // for example, if Task is a Doctrine entity, save it!
-            // $entityManager = $this->getDoctrine()->getManager();
-            // $entityManager->persist($task);
-            // $entityManager->flush();
-
-//            return $this->redirectToRoute('task_success');
+             $entityManager = $this->getDoctrine()->getManager();
+             $entityManager->persist($item);
+             $entityManager->flush();
         }
 
         return $this->render('views/item/new.html.twig', [
--- src/Form/Items/ItemType.php
+++ src/Form/Items/ItemType.php
@@ -6,8 +6,10 @@ namespace App\Form\Items;
 
 use App\Entity\Category;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query\Expr\Join;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -15,29 +17,49 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 class ItemType extends AbstractType
 {
+    private const CAT_GENRE_ID = 2;
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class)
-            ->add('description', TextareaType::class)
+            ->add('name', TextType::class, [
+                'attr' => ['class' => 'input'],
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => ['class' => 'input'],
+            ])
+            ->add('is_visible', CheckboxType::class, [
+                'label' => 'Privé',
+                'value' => true,
+                'attr' => ['class' => 'switch']
+            ])
             ->add('image_cover', FileType::class, [
-                'mapped' => false
+                'mapped' => false,
+                'required' => false,
+                'attr' => ['class' => 'input'],
             ])
             ->add('image_large', FileType::class, [
-                'mapped' => false
+                'mapped' => false,
+                'required' => false,
+                'attr' => ['class' => 'input'],
             ])
             ->add('categories', EntityType::class, [
                 'label' => 'Categories',
-                'required' => true,
                 'multiple' => true,
                 'expanded' => true,
                 'class' => Category::class,
                 'choice_label' => 'name',
                 'query_builder' => function(EntityRepository $er) {
                     return $er->createQueryBuilder('c')
+                        ->where('c.is_active = true')
+                        ->join(
+                            'c.parent',
+                            'cp',
+                            Join::WITH,
+                            "cp.id = " . self::CAT_GENRE_ID
+                        )
                         ->orderBy('c.name', 'ASC');
                 },
-
             ])
         ;
     }
